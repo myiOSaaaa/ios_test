3f6a663e955feb7a288cafbd13a5f2c36544fff2
t-inagaki <inagaki@pro.local>
2015-03-21 19:52:58 +0900

    Initial Commit

---
+++ PanGestureTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanGestureTest
+//
+//  Created by t-inagaki on 2015/03/21.
+//  Copyright (c) 2015年 イナガキ タカシ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PanGestureTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PanGestureTest
+//
+//  Created by t-inagaki on 2015/03/21.
+//  Copyright (c) 2015年 イナガキ タカシ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
